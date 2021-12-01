f2339d039913fe088de7fe843a51683ecab52283
anugoyal998 <anugoyal998@gmail.com>
2021-12-01 22:17:11 +0530

    move freehand

--- src/functions/handleMouseMove.js
+++ src/functions/handleMouseMove.js
@@ -16,10 +16,20 @@ export const handleMouseMove = (action,event,elements,setElements,tool,selectedE
     const { x1, y1 } = elements[index];
     updateElement(index,x1,y1,clientX,clientY,tool,elements,setElements);
   }else if(action === "moving"){
-    const {id,x1,x2,y1,y2,type} = selectedElement
-    const width = x2-x1
-    const height = y2-y1
-    updateElement(id,clientX,clientY,clientX+width,clientY+height,type,elements,setElements)
+    if(selectedElement.type === "pencil"){
+      const newPoints = selectedElement.points.map((_,index)=> ({
+        x: clientX - selectedElement.xOffsets[index],
+        y: clientY - selectedElement.yOffsets[index],
+      }))
+      const elementsCopy = [...elements]
+      elementsCopy[selectedElement.id].points = {...elementsCopy[selectedElement.id], points: newPoints}
+      setElements(elementsCopy,true)
+    }else{
+      const {id,x1,x2,y1,y2,type} = selectedElement
+      const width = x2-x1
+      const height = y2-y1
+      updateElement(id,clientX,clientY,clientX+width,clientY+height,type,elements,setElements)
+    }
   }else if(action === "resizing"){
     const {id,type,position,...coordinates} = selectedElement
     const {x1,y1,x2,y2} = resizedCoordinates(clientX,clientY,position,coordinates)
